fe4059de7bd2cb66afbef9c9f1bab28ee909f7af
Ewan132 <ewan132@yandex.ru>
2024-04-24 21:52:24 +0300

    Added a test

--- src/test/java/ManagerFilmsTest.java
+++ src/test/java/ManagerFilmsTest.java
@@ -6,19 +6,10 @@ import ru.netology.FilmManger.FilmManager;
 
 public class ManagerFilmsTest {
 
-
-    FilmManager manager = new FilmManager();
     Films film1 = new Films(1, "Бурлеск");
     Films film2 = new Films(2, "Маяк");
     Films film3 = new Films(3, "Солнцестояние");
-/*
-    @BeforeEach
-    public void setup(){
-        manager.addFilm(film1);
-        manager.addFilm(film2);
-        manager.addFilm(film3);
-
-    } */
+    Films film4 = new Films(4, "Таксист");
 
 
     @Test
@@ -35,21 +26,6 @@ public class ManagerFilmsTest {
         Assertions.assertArrayEquals(exp, act);
     }
 
-
-    @Test
-    public void lastFilm() {
-        FilmManager manager = new FilmManager();
-
-        manager.addFilm(film1);
-        manager.addFilm(film2);
-        manager.addFilm(film3);
-
-        Films[] exp = {film3, film2, film1};
-        Films[] act = manager.lastFilm();
-
-        Assertions.assertArrayEquals(exp, act);
-    }
-
     @Test
     public void lastFilmMinLimit() {
         FilmManager manager = new FilmManager();
@@ -62,6 +38,38 @@ public class ManagerFilmsTest {
         Films[] act = manager.lastFilm();
 
         Assertions.assertArrayEquals(exp, act);
+    }
+
+
+    @Test
+    public void lastFilmEqualLimit() {
+        FilmManager manager = new FilmManager();
+
+        manager.addFilm(film1);
+        manager.addFilm(film2);
+        manager.addFilm(film3);
+
+        Films[] exp = {film3, film2, film1};
+        Films[] act = manager.lastFilm();
+
+        Assertions.assertArrayEquals(exp, act);
+    }
+
+
+    @Test
+    public void lastFilmOverLimit() {
+        FilmManager manager = new FilmManager();
+
+        manager.addFilm(film1);
+        manager.addFilm(film2);
+        manager.addFilm(film3);
+        manager.addFilm(film4);
+
+        Films[] exp = {film4, film3, film2};
+        Films[] act = manager.lastFilm();
+
+        Assertions.assertArrayEquals(exp, act);
+
     }
 
 
